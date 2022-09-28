c34385d85242f9ba0703a2ca63c26ac28cec3f84
Dion Munipi <dion.munipi@kcl.ac.uk>
2022-09-28 15:07:50 +0100

    Write primes function

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def primesHelper(number):
+    for i in range(2,int(number**0.5)+1):
+        if number % i == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
     list = []
+    tester = 2
+    while len(list) != number_of_primes:
+        if (primesHelper(tester)):
+            list.append(tester)
+            tester += 1
+        else:
+            tester += 1
     return list
